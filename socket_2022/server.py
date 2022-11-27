8b4c0222736b89d55bd27e9c9564bde14a0283d8
Ngoc <dungtuyetngoc@gmail.com>
2022-11-27 22:10:07 +0700

    able to load index.html

--- socket_2022/server.py
+++ socket_2022/server.py
@@ -1,35 +1,41 @@
-import socket 
+import socket
+import base64
 # import threading
-import time
 
-BUFSIZE = 1024
+BUFFERSIZE = 10240
 PORT = 8080
 # HOST = socket.gethostbyname(socket.gethostname())
 HOST = ""
 ADDR = (HOST, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!quit"
+content_type_dict = {
+    "html": "text/html",
+    "htm": "text/html",
+    "txt": "text/plain",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "gif": "image/gif",
+    "png": "image/png",
+    "css": "text/css"
+}
 
-# create server socket
-# client connect server -> read http request
-# send http response
-# close server
-def create_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(ADDR) # bind this socket to this address so anything that connects to this ADDR (address) will hit this socket
-    server_socket.listen(5) # question
-    return server_socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# anything that connects to this ADDR (address) will hit this socket
+# bind this socket to this address
+server_socket.bind(ADDR)
 
 
 def read_request(client_socket, client_address):
     print(f"[NEW CONNECTION] {client_address} connected.")
     request = ""
-    client_socket.settimeout(1) # block socket operations for 1 sec
+    client_socket.settimeout(1)  # block socket operations for 1 sec
     try: 
-        request = client_socket.recv(BUFSIZE).decode()
+        request = client_socket.recv(BUFFERSIZE).decode()
         # keep reading if TCP request length > buffsize
-        while (request): 
-            request += client_socket.recv(BUFSIZE).decode()
+        while request:
+            request += client_socket.recv(BUFFERSIZE).decode()
     except client_socket.timeout: 
         if request == "": 
             print("DIDN'T RECEIVE DATA. [TIMEOUT]")
@@ -37,38 +43,50 @@ def read_request(client_socket, client_address):
         return request
 
 
-def start(server_socket): 
-    print(f"[LISTENING] Server is listening on {HOST}")
-    request = ""
-    while request == "": 
+def start():
+    global server_socket
+    print(f"[LISTENING] Server is listening")
+    server_socket.listen(5)
+    while True:
         client_socket, client_address = server_socket.accept()
         # thread = threading.Thread(target=read_request, args=(client_socket, client_address, request))
         # thread.start()
         request = read_request(client_socket, client_address)
-        return client_socket, request
+        # print(f"[HTTP REQUEST]\n{request}")
+        headers = request.split('\n')
+        # print(f"Headers: {headers}")
+        filename = headers[0].split()[1]
+        if filename == '/':
+            filename = '/index.html'
+        file_type = filename.split('.')[1]
+
+        with open('.' + filename, 'rb') as f:
+            content = base64.b64encode(f.read())
+
+        if file_type not in content_type_dict:
+            content_type = "application/octet-stream"
+        else:
+            content_type = content_type_dict[file_type]
+
+        # Send HTTP response
+        # without content-ranges, it cant even load image file
+        if content_type == "image/png":
+            response = """HTTP/1.1 200 OK
+                   Content-Length: %s
+                   Content-Type: %s
+                   Content-Ranges: bytes
+                   """ % (len(content), content_type)
+        else:
+            response = """HTTP/1.1 200 OK\r
+            Content-Length: %s\r
+            Content-Type: %s\r
+        """ % (len(content), content_type)
+
+        response = response.encode()
+        response += content
+        # print(f"[Response message] {response}")
+        client_socket.sendall(response)
+        # return client_socket, request
 
 
-def send_file_index(client_socket): 
-    with open("index.html", mode="rb") as index_file:# rb mode for non-text files
-        contents = index_file.read()
-    response_message = """HTTP/1.1 200 OK
-    Content-Length: %d
-
-    """%len(contents)
-    print(f"[HTTP Response Message Index.html]\n{response_message}")
-    response_message += contents.decode() # add body lines
-    client_socket.send(bytes(response_message, 'utf-8'))
-
-
-def load_page_index(server_socket, client_socket, request):
-    if "GET /index.html HTTP/1.1" in request or "GET / HTTP/1.1" in request:
-        send_file_index(client_socket)
-        server_socket.close()
-        return True
-
-    
-server_socket = create_server()
-client_socket, request = start(server_socket)
-print("[HTTP REQUEST]")
-print(request)
-load_page_index(server_socket, client_socket, request)
+start()
