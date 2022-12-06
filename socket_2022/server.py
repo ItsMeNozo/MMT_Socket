55e448645623a8186d7eccaf24f8936e0a4c5822
Ngoc <dungtuyetngoc@gmail.com>
2022-12-06 22:45:53 +0700

    fixed redundant code

--- socket_2022/server.py
+++ socket_2022/server.py
@@ -39,14 +39,12 @@ def handle_client(client_socket, client_address):
             request = client_socket.recv(server_config.BUFFERSIZE).decode()
         except socket.timeout:
             print("DIDN'T RECEIVE DATA. [TIMEOUT]")
-            client_socket.close()
             break
 
         if not request:
             # header = """HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"""
             header = """HTTP/1.1 200 OK\r\nConnection: close"""
             client_socket.send(header.encode())
-            client_socket.close()
             break
 
         if "POST" in request:
