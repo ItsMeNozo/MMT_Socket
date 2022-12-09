14b9b0f32405e848c0bc6305a9d42564e2735412
Ngoc <dungtuyetngoc@gmail.com>
2022-12-09 10:34:36 +0700

    nonconcurrent code worked

--- socket_2022/server.py
+++ socket_2022/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import server_config
+import os
 
 
 content_type_dict = {
@@ -16,21 +17,50 @@ content_type_dict = {
 }
 
 
+def read_request(client_socket):
+    request = ""  # set this for time out case
+
+    try:
+        req = client_socket.recv(server_config.BUFFERSIZE).decode()
+        while req:
+            request += req
+            req = client_socket.recv(server_config.BUFFERSIZE).decode()
+    except socket.timeout:
+        print("[TIMEOUT]")
+        if not request:
+            print("[DIDN'T RECEIVE REQUEST]")
+    return request
+
+
+def get_filename_content_type(headers):
+    filename = headers[0].split()[1]
+    if filename == '/':
+        filename = 'index.html'
+
+    if '.' in filename:
+        file_type = filename.split('.')[1]
+    else:
+        file_type = ""
+    if file_type not in content_type_dict:
+        content_type = "application/octet-stream"
+    else:
+        content_type = content_type_dict[file_type]
+    return filename, content_type
+
+
 def handle_client(client_socket, client_address):
     print(f"[NEW CONNECTION] {client_address} connected.")
 
     while True:
         # request = read_request(client_socket)
-        client_socket.settimeout(5)
+        request = ""
         try:
             request = client_socket.recv(server_config.BUFFERSIZE).decode()
         except socket.timeout:
-            print("DIDN'T RECEIVE DATA. [TIMEOUT]")
-            break
+            print("[TIMEOUT]")
 
         if not request:
-            # header = """HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"""
-            header = """HTTP/1.1 200 OK\r\nConnection: close"""
+            header = """HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"""
             client_socket.send(header.encode())
             break
 
@@ -42,33 +72,27 @@ def handle_client(client_socket, client_address):
         headers = request.split('\n')
         print(f"[HTTP REQUEST]\n{headers[0]}")
 
-        filename = headers[0].split()[1]
-        if filename == '/':
-            filename = 'index.html'
-
-        if '.' in filename:
-            file_type = filename.split('.')[1]
-        else:
-            file_type = ""
+        filename, content_type = get_filename_content_type(headers)
         # Return 404 when file does not exist
         try:
-            with open('.' + filename, 'rb') as f:
-                content = f.read()
+            f = open('.' + filename, 'rb')
+            f.seek(0, os.SEEK_END)
+            total = f.tell()
+            f.seek(0)  # go back to the start
+            data = f.read(server_config.BUFFERSIZE)
+            header = """HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n""" % (content_type, total)
+            client_socket.sendall(header.encode())
+
+            client_socket.send(data)
+            while data:
+                data = f.read(server_config.BUFFERSIZE)
+                client_socket.send(data)
+
+            f.close()
         except FileNotFoundError:
             response = "HTTP/1.1 404 NOT FOUND\r\nConnection: close\r\n\r\nFile Not Found"
             client_socket.sendall(response.encode())
             break
-
-        if file_type not in content_type_dict:
-            content_type = "application/octet-stream"
-        else:
-            content_type = content_type_dict[file_type]
-
-        # Send HTTP response
-        header = """HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n""" % (content_type, len(content))
-
-        client_socket.sendall(header.encode() + content)
-        # client_socket.send(content)
 
     client_socket.close()
     print(f"[NEW CONNECTION] {client_address} closed.")
@@ -76,20 +100,19 @@ def handle_client(client_socket, client_address):
 
 def accept_incoming_connections(server_socket):
     """Sets up handling for incoming clients."""
-    while True:
-        client_socket, client_address = server_socket.accept()
-        request_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        # request_thread.daemon = True
-        request_thread.start()
-        request_thread.join()
 
-        print(f"[NEW CONNECTION] {client_address} closed.")
+    # while True:
+    #     client_socket, client_address = server_socket.accept()
+    #     client_socket.settimeout(20)
+    #     request_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    #     request_thread.start()
 
-    # # Non-concurrency code:
-    # client_socket, client_address = server_socket.accept()
-    # # request_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-    # handle_client(client_socket, client_address)
-    # print(f"[NEW CONNECTION] {client_address} closed.")
+    # Non-concurrency code:
+    client_socket, client_address = server_socket.accept()
+    # request_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    client_socket.settimeout(5)  # block socket operations for 5 sec
+    handle_client(client_socket, client_address)
+
 
 def check_login(request, client_socket):
     if "uname=admin&psw=123456" not in request:
